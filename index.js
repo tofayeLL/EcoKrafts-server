dd32be21b387da9614c2797dbb304e662f3565d1
tofayeLL <ahmedtofayel132@gmail.com>
2024-04-28 02:15:23 +0600

    By  Use Delete method delte item from the database

--- index.js
+++ index.js
@@ -58,6 +58,15 @@ async function run() {
         })
 
 
+        // DELETE
+        app.delete('/myCrafts/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await craftCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
 
 
 
